782907d8bad66d2519af9944f109f63d61d23d48
JasonMetal <935216773@qq.com>
2021-11-12 10:51:28 +0800

    update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -20,6 +20,7 @@ module.exports = {
                     '/javaScript/dataType',
                     '/javaScript/variable',
                     '/javaScript/function',
+                    '/javaScript/functions_diy',
                     '/javaScript/environment',
                     '/javaScript/operating-mechanism',
                     '/javaScript/closure',
